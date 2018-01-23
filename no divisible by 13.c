699ba81d7b9d73a1d0620a7f53ce28f096949584
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-23 11:16:21 +0000

    Create no divisible by 13.c

---
+++ no divisible by 13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	  int no;
+	  scanf("%d",&no);
+	  if(no%13==0)
+	  printf("no is divisible by 13");
+	  else
+	  	  printf("no is not divisible by 13");
+return 0;
+}
