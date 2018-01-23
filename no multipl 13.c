bb90163cbfb0ee8ade0a88017349773f4189862e
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-23 11:17:59 +0000

    Update and rename no divisible by 13.c to no multipl 13.c

--- no multipl 13.c
+++ no multipl 13.c
@@ -4,8 +4,8 @@ int main()
 	  int no;
 	  scanf("%d",&no);
 	  if(no%13==0)
-	  printf("no is divisible by 13");
+	  printf("no is  multiples of 13");
 	  else
-	  	  printf("no is not divisible by 13");
+	  	  printf("no is not multiple of 13");
 return 0;
 }
